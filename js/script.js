97330575d6dc16cb9898d46aaba05ba2f94c3ca1
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-11-08 19:34:05 +0600

    toDo list

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let btn =document.getElementById("btnSubmit");
+let input=document.getElementById("inputField");
+let unoderList=document.getElementById("unoderList");
+
+
+
+btn.addEventListener("click",()=>{
+    let inputText= input.value;
+
+    if(inputText === ""){
+
+        alert("Write Something");
+
+    }else{
+        let li =document.createElement("li");
+        li.innerHTML =inputText;
+        li.style.border="1px solid black";
+        li.style.padding="5px";
+        li.style.marginTop="5px";
+        li.style.marginBottom="5px";
+        let deleteBtn=document.createElement("button");
+        deleteBtn.innerHTML="Delete";
+        deleteBtn.style.background="#8E8F64";
+        deleteBtn.style.paddingTop="10px";
+        deleteBtn.style.paddingBottom="10px";
+        deleteBtn.style.paddingLeft="30px";
+        deleteBtn.style.paddingRight="30px";
+        deleteBtn.style.borderRadius="30px";
+        deleteBtn.style.marginLeft="30px";
+        deleteBtn.addEventListener("click",()=>{
+            li.remove();
+        })
+        li.appendChild(deleteBtn);
+        unoderList.appendChild(li);
+
+          input.value = "";
+
+    }
+
+    
+
+    
+})
+
+
